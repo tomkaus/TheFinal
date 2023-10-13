7c0964d7a865e3f5c525741d36348eaa4e1e75eb
tom kaus <landtkaus@aol.com>
2023-10-12 20:53:02 -0600

    update 2

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import './Course.css'; // Import the CSS file
-import image from './Images/Instructor.jpg'
+import './Course.css';
+import image from './Images/Instructor.jpg';
 
 export default function Course() {
   return (
     <div className="course-container">
-      <div className="instructor-info">
-        <img
-          src={image}
-          alt="Your Image"
-          className="instructor-image"
-        />
-        <div className="instructor-description">
-          <h2>About this Course</h2>
-          <p className="course-description">
-            Welcome to the Intermediate Algebra course! In this course, you will
-            learn fundamental concepts of algebra and build a strong foundation
-            for more advanced mathematical topics.
-          </p>
-        </div>
+      <div className="course-info">
+        <h2>About this Course</h2>
+        <p className="course-description">
+          Welcome to the Intermediate Algebra course! In this course, you will
+          learn fundamental concepts of algebra and build a strong foundation
+          for more advanced mathematical topics. Please login, make sure you type in your class section.
+           Watch the lessons and take the quiz. Come back to the login page and tell me how you did.
+        </p>
         <h2>Instructor Info</h2>
         <p>
           I am Tom Kaus, your instructor, and I am passionate about helping
           students succeed in their mathematical journey.
         </p>
+      </div>
+      <div className="instructor-info">
+        <img src={image} alt="Your Image" className="instructor-image" />
       </div>
     </div>
   );
